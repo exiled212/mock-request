c7904a7ffeeceba92de7c586722a615f2fcb5a87
Daniel Ruiz <daniel.ruiz@rappi.com>
2021-10-19 03:33:14 -0500

    chg: update README

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,16 @@ import { config } from 'dotenv';
 async function bootstrap() {
 	config();
 
-	const env = process.env.NODE_ENV;
-
-	if (env) {
-		await createConnection({
-			type: 'postgres',
-			host: process.env.DB_HOST,
-			port: process.env.DB_PORT as unknown as number,
-			database: process.env.DB_NAME,
-			username: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
-			synchronize: true,
-			entities: [Request, Response],
-		});
-	}
+	await createConnection({
+		type: 'postgres',
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT as unknown as number,
+		database: process.env.DB_NAME,
+		username: process.env.DB_USER,
+		password: process.env.DB_PASSWORD,
+		synchronize: true,
+		entities: [Request, Response],
+	});
 	getConnection();
 
 	const app = await NestFactory.create(AppModule);
